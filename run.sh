f90ca42cf8119a80c1a0877dcb17c040862f102a
Ozpyn <74064734+Ozpyn@users.noreply.github.com>
2026-02-19 13:32:50 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ npm install &&
 npm run build &&
 cd ../srv &&
 python3 -m venv .venv &&
-(. .venv/bin/activate 2>/dev/null || . .venv/Scripts/activate) &&
+source .venv/bin/activate &&
 pip install -r requirements.txt &&
 python3 app.py'
